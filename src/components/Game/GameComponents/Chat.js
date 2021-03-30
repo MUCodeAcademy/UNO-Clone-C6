14bc5b66b7bce7bbd20d5cdefc77d1191161d2f9
Joshua Dale <jdale9930@gmail.com>
2021-03-30 16:48:27 -0500

    chat.js functional complete

--- src/components/Game/GameComponents/Chat.js
+++ src/components/Game/GameComponents/Chat.js
@@ -1,17 +1,14 @@
 import {React, useEffect, useState} from "react"
 import useChat from '../../../hooks/useChat.js'
 
-const Chat = () => {
+const Chat = (props) => {
 
+    //Chat will need the Username and the Room passed through props
     const [msg, setMsg] = useState("");
     const [username, setUsername] = useState("");
     const [loggedIn, setLoggedIn] = useState(false);
     const {messages, sendMessage, joinRoom} = useChat(username, "room");
-    // const [messageDisplay, setMessageDisplay] = useState([])
-    // // const {joinRoom} = useChat(joinRoom)
 
-    // useEffect(()=>{setMessageDisplay(messages)},[messages])
-// console.log(messages)
 return(<div>
 
     {!loggedIn ? <>
@@ -23,18 +20,16 @@ return(<div>
         <button onClick = {() =>{if(username){setLoggedIn(true)}}}>Submit</button>
     </div>
     </> :
-    <>
-    <div className = "chatField" style = {{height: "300px",width: "300px", border: "2px solid black"}}>
-        Yo messages length{messages.length}
-        {messages.length > 0 && 
-        messages.map((m)=>{
-            <div>
-                <b>{m.username}:</b>
-                {m.body}
-            </div>
+    <div>
+        {messages[0].length > 0 && 
+        messages[0].map((m, idx)=>{
+            return(
+            <div key={idx}>
+                <b>{m.username}</b>
+                <div>{m.body}</div>
+            </div>)
         })
-        }
-    </div>
+    }
     <div className = "inputField">
         <textarea rows = "6" cols = "50" value = {msg}
         onChange = {(evt)=>{setMsg(evt.target.value)}}></textarea>
@@ -45,8 +40,10 @@ return(<div>
         }}}
         >Send</button>
         <button onClick = {()=>{joinRoom()}}>Join Room</button>
+        <button onClick = {()=>{console.log(messages); console.log(messages[0].length)}}>log messages</button>
+
     </div>
-    </>}
+    </div>}
 </div>)
 }
 
