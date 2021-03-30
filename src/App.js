2900479f99f491fe06bb69d1aac6dc31dc2ac152
michaelivers909 <michael.ivers@midlandu.edu>
2021-03-30 12:31:39 -0500

    Routing Finished (for now)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useContext } from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  useHistory,
+  Redirect,
+} from "react-router-dom";
+import GamePage from "./components/Game/GamePage";
+import HomePage from "./components/Home/HomePage";
+import UserSettingsPage from "./components/UserSettings/UserSettingsPage";
+import LoginPage from "./components/Login/LoginPage";
+import SignupPage from "./components/Signup/SignupPage";
+import ResetPasswordPage from "./components/ResetPassword/ResetPasswordPage";
+import ProtectedRoutes from "./shared/ProtectedRoutes";
+import { UserContext } from "./shared/UserContext";
 
 function App() {
+  const { currentUser } = useContext(UserContext);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        {/* <ProtectedRoutes currentUser={currentUser} path="/Login" authRequired={false} component={LoginPage} /> */}
+        {/* <ProtectedRoutes currentUser={currentUser} path="/Signup" authRequired={false} component={SignupPage} /> */}
+        <ProtectedRoutes
+          currentUser={currentUser}
+          path="/ResetPassword"
+          authRequired={false}
+          component={ResetPasswordPage}
+        />
+        {/* <ProtectedRoutes currentUser={currentUser} path="/UserSettings" authRequired={true} component={UserSettingsPage} /> */}
+        {/* <ProtectedRoutes currentUser={currentUser} path="/" authRequired={true} component={HomePage} /> */}
+        {/* <ProtectedRoutes currentUser={currentUser} path="/GamePage" authRequired={true} component={GamePage} /> */}
+        {/* <Route path = "*"> */}
+        {/* <Redirect to = "/Login" /> */}
+        {/* </Route> */}
+      </Switch>
+    </Router>
   );
 }
 
