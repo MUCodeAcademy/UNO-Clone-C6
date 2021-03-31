cd3ab9a774677ef86a8daa7af5ab569f52926af4
bedebates <bendebates@gmail.com>
2021-03-31 10:02:15 -0500

    functionality for usersetting haven't test context

--- src/components/UserSettings/UserSettingsPage.js
+++ src/components/UserSettings/UserSettingsPage.js
@@ -1,6 +1,7 @@
 import { Button, Grid, makeStyles, Paper, TextField } from "@material-ui/core";
 import React, { useState } from "react";
-// import TextField from '@material-ui/core/TextField';
+import { useHistory } from "react-router";
+// import { AuthContext } from "../contexts/AuthContext";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -17,20 +18,69 @@ const useStyles = makeStyles((theme) => ({
     textAlign: "center",
     color: theme.palette.text.secondary,
   },
+  testErr: {
+    color: "red",
+  },
 }));
 
 export default function UserSettingsPage() {
   const classes = useStyles();
   console.log(classes);
-  const originalEmail = "test@test.com";
+  const originalEmail = "test@test.com"; //need a way to get users original email
   const originalPassword = "*********";
-  const originalUsername = "Username";
+  const originalUsername = "Username"; //need a way to get users original username
   const [email, setEmail] = useState(originalEmail);
   const [password, setPassword] = useState(originalPassword);
   const [passwordConfirm, setPasswordConfirm] = useState(originalPassword);
   const [username, setUsername] = useState(originalUsername);
+  const [errors, setErrors] = useState("");
+  const [loading, setLoading] = useState(false);
+  const {
+    currentUser,
+    updateEmail,
+    updatePassword,
+    updateUsername,
+  } = useContext(AuthContext);
+  const history = useHistory(0);
+  function handleSubmit(e) {
+    setErrors("");
+    e.preventDefault();
+    if (password !== passwordConfirm) {
+      return setErrors("Passwords must match");
+    }
+    if (!/\S+@\S+\.\S+/.test(email)) {
+      return setErrors("Please enter a valid email");
+    }
+    if (username < 4) {
+      return setErrors("Username must be more than 4 charectors");
+    }
+    const upDates = [];
+    setLoading(true);
+    setErrors("");
 
-  // let email = "test@test.com";
+    if (email !== originalEmail) {
+      upDates.push(updateEmail(email));
+    }
+
+    if (password !== originalPassword) {
+      upDates.push(updatePassword(password));
+    }
+    if (username !== originalUsername) {
+      upDates.push(updateUsername(username));
+    }
+
+    Promise.all(upDates)
+      .then(() => {
+        history.push("/Create");
+      })
+      .catch(() => {
+        setErrors("Failed to update your account.");
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }
+
   return (
     <form noValidate autoComplete="off">
       <div className={classes.root}>
@@ -78,6 +128,7 @@ export default function UserSettingsPage() {
           <Grid item xs={5} sm={4}>
             <TextField
               id="outlined-basic"
+              type="password"
               label={`New password ${
                 password !== originalPassword ? "*Changed" : ""
               }`}
@@ -104,6 +155,7 @@ export default function UserSettingsPage() {
               id="outlined-basic"
               label="Confirm password"
               variant="outlined"
+              type="password"
               defaultValue={originalPassword}
               onChange={(e) => {
                 setPasswordConfirm(e.target.value);
@@ -151,25 +203,21 @@ export default function UserSettingsPage() {
           justify="flex-end"
         >
           <Grid item xs={7} sm={4}>
-            <Button variant="contained" color="primary">
+            <Paper className={classes.paper} className={classes.testErr}>
+              {errors}
+            </Paper>
+          </Grid>
+          <Grid item xs={7} sm={4}>
+            <Button
+              onClick={handleSubmit}
+              disabled={loading}
+              variant="contained"
+              color="primary"
+            >
               Submit changes
             </Button>
           </Grid>
         </Grid>
-        {/* <Button variant="contained" color="primary">
-          Submit changes
-          to {password !== originalPassword && "password"}{" "}
-          {(email !== originalEmail) +
-            (password !== originalPassword) +
-            (username !== originalUsername) >=
-            2 && " and "}{" "}
-          {email !== originalEmail && "email"}
-          {(email !== originalEmail) +
-            (password !== originalPassword) +
-            (username !== originalUsername) >=
-            3 && " and "}
-          {username !== originalUsername && "username"}
-        </Button> */}
       </div>
     </form>
   );
---
+++ view_code.md
@@ -0,0 +1,126 @@
+```javascript
+
+
+import React, { useRef, useState, useContext } from "react";
+import { UserContext } from "../../shared/UserContext";
+
+
+
+
+
+export default function Signup() {
+    const emailRef = useRef();
+    const passwordRef = useRef();
+    const passwordConfirmRef = useRef();
+    const userNameRef = useRef();
+    const { signUp } = useContext(AuthContext);
+    const [ error, setError ] = useState("");
+    const [ loading, setLoading ] = useState(false);
+    const history = useHistory();
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+        if (passwordRef.current.value !==
+        passwordConfirmRef.current.value) {
+            return setError("Passwords must match");
+    }
+    try {
+        setError ("");
+        setLoading(true);
+        await signUp(emailRef.current.value, passwordRef.current.value, userNameRef.current.value);
+        // history.push("/");
+    } catch {
+        setError ("Something went wrong, unable to create account. Please try again.");
+    }
+        setLoading(false);
+    }
+
+    return (
+_______________________________________________________________________________________________
+
+
+import React, { useRef, useState, useContext } from "react";
+import { Form, Button, Card, Alert, Container } from "react-bootstrap";
+import { AuthContext } from "../contexts/AuthContext";
+import { Link, useHistory } from "react-router-dom";
+import "../Style.css";
+
+export default function UpdateProfile() {
+    const emailRef = useRef();
+    const passwordRef = useRef();
+    const passwordConfirmRef = useRef();
+    const { currentUser, updateEmail, updatePassword } = useContext(AuthContext);
+    const [ error, setError ] = useState("");
+    const [ loading, setLoading ] = useState(false);
+    const history = useHistory();
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        if (passwordRef.current.value !==
+        passwordConfirmRef.current.value) {
+            return setError("Passwords must match");
+    }
+
+        const upDates = []
+        setLoading(true);
+        setError("");
+
+        if (emailRef.current.value !== currentUser.email) {
+        upDates.push(updateEmail(emailRef.current.value));
+        }
+
+        if (passwordRef.current.value) {
+            upDates.push(updatePassword(passwordRef.current.value));
+        }
+
+        Promise.all(upDates).then(() => {
+            history.push("/Create")
+        }).catch(() => {
+            setError("Failed to update your account.");
+        }).finally(() => {
+            setLoading(false);
+        })
+    };
+
+    return (
+        <body className="bgParchGrey">
+        <Container
+            className="justify-content-center"
+            style={{ minHeight: "100vh", maxWidth: 600}}
+        >
+        <Card border="secondary" className="bgParchGrey fontM">
+            <Card.Body className="text-center mb-4 mt-4">
+                <h2 className="fontDafoe">Update Profile</h2>
+                {error && <Alert variant="danger">{error}</Alert>}
+                <Form onSubmit={handleSubmit}>
+                    <Form.Group id="email">
+                        <Form.Label>Email</Form.Label>
+                        <Form.Control type="email" ref={emailRef}
+                        defaultValue={currentUser.email}/>
+                    </Form.Group>
+                    <Form.Group id="password">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" ref={passwordRef}
+                        placeholder="Leave blank to keep current password."/>
+                    </Form.Group>
+                    <Form.Group id="passwordConfirm">
+                        <Form.Label>Password Confirmation</Form.Label>
+                        <Form.Control type="password" ref={passwordConfirmRef}
+                        placeholder="Leave blank to keep current password."/>
+                    </Form.Group>
+                    <Button variant="info" disabled={loading} className="w-50" type="submit">Update Account</Button>
+                </Form>
+                <div className="w-100 text-center mt-4">
+                <Link to="/Create">Cancel</Link>
+                </div>
+            </Card.Body>
+        </Card>
+        </Container>
+        </body>
+
+
+
+    )
+
+}
+```
