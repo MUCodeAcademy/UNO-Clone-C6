d7e480f2a62c2df29ca5d830461e9494ed060bcc
conor-mcgowan <conor.mcgowan@live.com>
2021-04-02 14:27:30 -0500

    adds ability for socket to populate playerArray

--- src/shared/GameContext.js
+++ src/shared/GameContext.js
@@ -8,39 +8,65 @@ export function GameProvider({ children }) {
   const [canPlay, setCanPlay] = useState[false];
   const [playerArray, setPlayerArray] = useState[[]];
   const [gameActive, setGameActive] = useState[false];
+  const [cards, setCards] = useState[deck];
   const [drawDeck, setDrawDeck] = useState[[]];
   const [discardDeck, setDiscardDeck] = useState[[]];
+  const [userInfo, setUserInfo] = useState[{}];
 
-  useEffect(() => {
-    if (playerArray.length === 1) {
-      setIsHost(true);
-    }
-    [];
-  });
-
-  function shuffleDrawDeck(draw) {
-    for (let i = 0; i < draw.length; i++) {
-      let j = Math.floor(Math.random() * i);
-      let temp = draw[i];
-      draw[i] = draw[j];
-      draw[j] = temp;
-    }
+  function createUserInfo(providedName) {
+    let ID =
+      Math.random().toString(36).substring(2, 4) +
+      Math.random().toString(36).substring(2, 6);
+    setUserInfo({ username: providedName, userID: ID });
   }
 
-  function deal() {
-    let shuffledDeck = shuffleDrawDeck(deck);
+  function oneShuffle(toShuffle) {
+    let pre = [...toShuffle];
+    let post = [];
+    for (let i = 0; i <= pre.length; i++) {
+      post.splice(Math.floor(Math.random() * i), 0, pre[i]);
+    }
+    let almost = post.reverse();
+    let pen = [];
+    for (let i = 0; i <= almost.length; i++) {
+      pen.splice(Math.floor(Math.random() * i), 0, almost[i]);
+    }
+    let ult = pen.filter((x) => x !== undefined);
+    return ult;
+  }
+
+  function shuffleDeck(toBeShuffled) {
+    let shuffled = oneShuffle(
+      oneShuffle(
+        oneShuffle(oneShuffle(oneShuffle(oneShuffle(oneShuffle(toBeShuffled)))))
+      )
+    );
+    setCards(shuffled);
+  }
+
+  function deal(toBeDealt) {
+    let shuffled = shuffleDeck([...cards]);
     for (let i = 0; i < playerArray.length; i++) {
-      for (let j = 1; j <= 4; j++) {
-        let dealt = shuffledDeck.shift();
-        let dealee = playerArray[i];
-        dealee.push(dealt);
+      let newPlayerArray = [...playerArray];
+      for (let j = 1; j <= 7; j++) {
+        let dealtCard = shuffled.shift();
+        newPlayerArray[i] = {
+          ...newPlayerArray[i],
+          hand: { ...newPlayerArray[i].hand, dealtCard },
+        };
+        setPlayerArray(...newPlayerArray);
       }
     }
-    shuffledDeck.forEach((card) => {
+    shuffled.forEach((card) => {
       draw.push(card);
     });
     shuffledDeck.splice(0);
     discard.push(draw.shift());
+  }
+
+  function startGame() {
+    let gameCards = shuffleDeck();
+    deal(gameCards);
   }
 
   function regularTurn() {
