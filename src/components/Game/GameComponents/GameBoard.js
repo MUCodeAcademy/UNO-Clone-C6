b0b418eea568b1b240ada0d22f8e9fe09f27109b
Jonathan Penry <jonathan.penry@gmail.com>
2021-04-06 14:52:26 -0500

    Co-authored-by: Sara Bertuldo <sarabertuldo@users.noreply.github.com>

--- src/components/Game/GameComponents/GameBoard.js
+++ src/components/Game/GameComponents/GameBoard.js
@@ -1,34 +1,64 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useContext } from "react";
+import {GameContext} from '../../../shared/GameContext';
 import Button from '@material-ui/core/Button';
 import { Alert } from '@material-ui/lab';
 import Countdown from 'react-countdown';
 import "./GameBoard.css"
+import {Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle} from '@material-ui/core';
 
-
-const GameBoard = (props) => {
-    const [gameActive, setGameActive] = useState("");
+ const GameBoard = (props) => {
     const [msg, setMsg] = useState("")
     const [drawedCard, setDrawedCard] = useState(false)
+    const [open, setOpen] = useState(false);
+    const renderer = ({ hours, minutes, seconds, completed }) => {
+        if (completed) {
+        } else {
+          return <span>{seconds}</span>;}
+        }
+
+
+    const {drawCard, canPlay, isHost, startGame, quitGame, regularTurn, setColor, gameActive, setGameActive} = useContext(GameContext)
 
     useEffect(() => {
         setTimeout(() => {
-            setMsg(<Alert severity="error" className="alert">You have {<Countdown date={Date.now() + 30000} />}
-              to make a choice or lose a turn!</Alert>);
+            setMsg(<Countdown renderer={renderer} date={Date.now() + 3000} />);
+            console.log(msg)
+                setOpen(true)
             setTimeout(() => {
                 drawCard()
-                regularTurn()
-            }, 30000)
-        }, 90000)
-    }, [canPlay]);
+            }, 30500)
+        }, 2000)
+    }, [
+        canPlay
+    ]);
+
 
 
     return (
         <div className="boardContainer">
+        <Dialog
+        open={open}
+
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">{"Player Timeout"}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+          <Alert severity="error" className="alert">You have {msg} to make a choice or lose a turn!</Alert>
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={()=>{setOpen(false)}} color="primary">
+            OK
+          </Button>
+        </DialogActions></Dialog>
 
             <div className="thebigthree">
                 <div className="drawContainer">
-                    <div style={{ backgroundColor: "black" }} className={`cardback`} onClick={() => { drawCard(); setDrawedCard(true) }}>
-                        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/UNO_Logo.svg/1280px-UNO_Logo.svg.png" width="100px" height="60px" className="logo" />
+                    <div style={{ backgroundColor: "black" }} className={`cardback`} onClick={() => { 
+                        drawCard(); setDrawedCard(true) 
+                        }}>
                     </div>
                 </div>
 
@@ -40,30 +70,42 @@ const GameBoard = (props) => {
                     {isHost !== false && gameActive !== true &&
                         <div className="startDiv">
                             <div className="startButton"
-                                onClick={() => { setGameActive(); startGame(); }}>
+                                onClick={() => { 
+                                    setGameActive(); startGame(); 
+                                    }}>
                                 <p>GAME START</p>
                             </div>
                         </div>
-                    }
+                   }
 
-                    {isHost !== true && gameActive !== true &&
+                    {isHost !== true && gameActive !== true && 
                         <div className="text">
                             Waiting on host...
-                         </div>
-                    }
+                         </div>}
+                    
 
                     {drawedCard && <Button variant="contained"
                         color="primary"
                         value="regularTurn"
                         className="regularTurn"
-                        onClick={() => { regularTurn(); setDrawedCard(false) }}
+                        onClick={() => { 
+                            regularTurn(); setDrawedCard(false) 
+                        }}
                     >Next Turn</Button>}
 
                     <div className="color-area">
-                        <div className="color-box" style={{ backgroundColor: "blue" }} onClick={() => setColor("blue")}></div>
-                        <div className="color-box" style={{ backgroundColor: "green" }} onClick={() => setColor("green")}></div>
-                        <div className="color-box" style={{ backgroundColor: "red" }} onClick={() => setColor("red")}></div>
-                        <div className="color-box" style={{ backgroundColor: "yellow" }} onClick={() => setColor("yellow")}></div>
+                        <div className="color-box" style={{ backgroundColor: "blue" }} 
+                        onClick={() => setColor("blue")}
+                        ></div>
+                        <div className="color-box" style={{ backgroundColor: "green" }} 
+                        onClick={() => setColor("green")}
+                        ></div>
+                        <div className="color-box" style={{ backgroundColor: "red" }} 
+                        onClick={() => setColor("red")}
+                        ></div>
+                        <div className="color-box" style={{ backgroundColor: "yellow" }} 
+                        onClick={() => setColor("yellow")}
+                        ></div>
                     </div>
                 </div>
             </div>
@@ -86,5 +128,4 @@ const GameBoard = (props) => {
         </div >
     )
 }
-
 export default GameBoard;
