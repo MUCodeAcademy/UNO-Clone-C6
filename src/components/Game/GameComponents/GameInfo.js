61f85b1c687808140639b90e2ccb0689bb48cdc4
Joshua Dale <jdale9930@gmail.com>
2021-04-08 11:03:53 -0500

    styling finished

--- src/components/Game/GameComponents/GameInfo.js
+++ src/components/Game/GameComponents/GameInfo.js
@@ -1,12 +1,27 @@
 import React, { useContext } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Box from "@material-ui/core/Box";
 import { GameContext } from "../../../shared/GameContext";
 import Container from "@material-ui/core/Container";
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    backgroundColor: "rgba(0, 0, 0, 0.2)",
+    height: "100%",
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "center",
+    boxShadow: "inset 0px 0px 10px 5px rgba(0,0,0, 0.5)"
+  },
+}));
+
 const GameInfo = () => {
+  const classes = useStyles();
   const { playerArray, discardDeck, gameActive, isHostCon,  } = useContext(GameContext);
 
   return (
-    <Container>
+    <Container className = {classes.root}>
       {gameActive === false && (
         <>
           {isHostCon === false ? 
@@ -16,12 +31,11 @@ const GameInfo = () => {
       )}
 
       {gameActive === true && (
-        <>
-          <button onClick = {()=>{console.log(playerArray)}}></button>
+        <div>
           <h3>{playerArray[0] && `${playerArray[0].username}'s`} turn!</h3>
-          <p>The color is {discardDeck[0] && `${discardDeck[0].color}`}</p>
-          <p>{playerArray[1] && `${playerArray[1].username}`} is next!</p>
-        </>
+          <div>The current color is:  <div style = {{color: `${discardDeck[0].color}`, fontWeight: "bold"}}>{discardDeck[0] && `${discardDeck[0].color.toUpperCase()}`}</div></div>
+          <div>{playerArray[1] && `${playerArray[1].username}`} is next!</div>
+        </div>
       )}
     </Container>
   );
