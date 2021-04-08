30f6dff0f9487bc5ec16565de65c8b308f807fe0
Joshua Dale <jdale9930@gmail.com>
2021-04-08 10:30:01 -0500

    succesful host check

--- src/components/Game/GameComponents/GameInfo.js
+++ src/components/Game/GameComponents/GameInfo.js
@@ -3,18 +3,21 @@ import { GameContext } from "../../../shared/GameContext";
 import Container from "@material-ui/core/Container";
 
 const GameInfo = () => {
-  const { playerArray, discardDeck, gameActive } = useContext(GameContext);
+  const { playerArray, discardDeck, gameActive, isHostCon,  } = useContext(GameContext);
 
   return (
     <Container>
-      {!gameActive && (
+      {gameActive === false && (
         <>
-          <h3>Game has not started yet...</h3>
+          {isHostCon === false ? 
+          <h3>Waiting for host to start game...</h3>
+          :<h3>Start game when you are ready!</h3>}
         </>
       )}
 
-      {gameActive && (
+      {gameActive === true && (
         <>
+          <button onClick = {()=>{console.log(playerArray)}}></button>
           <h3>{playerArray[0] && `${playerArray[0].username}'s`} turn!</h3>
           <p>The color is {discardDeck[0] && `${discardDeck[0].color}`}</p>
           <p>{playerArray[1] && `${playerArray[1].username}`} is next!</p>
